d32404e0333c05d2fc5c3d9120709302f4ad338e
pg <ang-st@kobe.home>
2022-05-10 21:08:21 +0200

    test: add top-up operations

--- test/02_top_up.js
+++ test/02_top_up.js
@@ -2,11 +2,22 @@ var assert = require('assert');
 const faker = require('@faker-js/faker').faker
 const Client = require('../src/ips_client')
 let client = null
+
 beforeEach(async function () {
   client= new Client()
 
 });
 
+
+const fakeMID = async function(){
+  const payload = {"mid": Date.now(),
+  "name":faker.company.companyName(),
+   "address":faker.address.streetAddress(),
+   "city":faker.address.city(),
+   "postal_code":faker.address.zipCode(), "active":true}
+  
+  return client.createMID(payload)  
+}
 
 describe('Top-up', function () {
   describe('faucet', function () {
@@ -16,7 +27,7 @@ describe('Top-up', function () {
        assert('code' in res);
     })
 
-    it('can also make killed voucher', async function () {
+    it('can also generate killed voucher', async function () {
        const res = await client.generateVoucher(true)
 
        assert('code' in res);
@@ -27,19 +38,49 @@ describe('Top-up', function () {
   describe('confirm', function () {
     it('can confirm a valid voucher', async function () {
       
-      const res = await client.generateVoucher()
+      const mid= await fakeMID()
+      const voucher = await client.generateVoucher()
+      const res= await client.confirmVoucher(voucher.code, mid.mid) 
 
-      assert('code' in res);
+      assert(res.success, true);
    })
     it('reject a killed voucher', async function () {
-      
-      const res = await client.generateVoucher(true)
-      
+      const mid= await fakeMID()
+      const voucher = await client.generateVoucher(true)
+      const res= await client.confirmVoucher(voucher.code, mid.mid) 
 
-      assert('code' in res);
+      assert.equal(res.success, false);
    })
+  })
+  describe('check', function () {
+    it('is available upon voucher creation', async function () {
+      const mid= await fakeMID()
+      const voucher = await client.generateVoucher()
+      const res= await client.checkVoucher(voucher.code) 
+      
+      assert.equal(res.success, true);
+      assert.equal(res.data.code, voucher.code)
+    
+    })
+
+    it('update after voucher validation', async function () {
+
+      const mid= await fakeMID()
+      const voucher = await client.generateVoucher()
+      const status = await client.checkVoucher(voucher.code) 
+      
+      assert.equal(status.data.billed, false)
+    
+      await client.confirmVoucher(voucher.code, mid.mid) 
+      const updated = await client.checkVoucher(voucher.code) 
+      
+      assert.equal(updated.data.billed, true)
+      assert.equal(updated.data.code, status.data.code)
 
 
+    })
+  
+  
   })
 
 
